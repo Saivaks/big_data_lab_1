2fb4b0657136df5ea8d00edb9c725dd8b672a413
unknown <tik11994@mail.ru>
2023-04-25 01:57:49 +0300

    Changes to be committed:

--- test/test_len.py
+++ test/test_len.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import os
-path_data = r'S:\andrey\мага\sem_2\big_data\big_data_lab_1\data'
-path_result = r'S:\andrey\мага\sem_2\big_data\big_data_lab_1'
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini', encoding="utf-8")
+
+path_data = config['DATA']['path_test_data']
+path_result = config['DATA']['path_test_result']
 df_orig = pd.read_csv(os.path.join(path_data, 'BBC News Test.csv'))
 df_result = pd.read_csv(os.path.join(path_result, 'result.csv'))
 def test_answer():
